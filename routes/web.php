eb794a73dfe94e9ffee30dce4f1b6365e585c8fa
Ivica <ivica_radic85@hotmail.com>
2023-09-21 12:55:29 +0200

    Fixing Laravel auth layout

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/','pages.home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
