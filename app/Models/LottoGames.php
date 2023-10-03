eb9d430403f9e7fa96b34a6a8345b98d783ba014
Ivica <ivica_radic85@hotmail.com>
2023-10-03 12:12:27 +0200

    * Start lotto games command

---
+++ app/Models/LottoGames.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LottoGames extends Model
+{
+    use HasFactory;
+
+    protected $table = 'lotto_games';
+
+    protected $fillable = [
+      'numbers',
+      'award_fund',
+      'jackpot',
+    ];
+}
