ee7286ec0fcfb537d6c2cb8ede2bf8f686db361a
Ivica <ivica_radic85@hotmail.com>
2023-10-03 11:49:23 +0200

    * Lotto games migrations && model

---
+++ app/Rules/SufficientCredits.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Auth;
+
+class SufficientCredits implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if(Auth::user()->credits < env ('TICKET_PRICE_CREDITS')){
+            $missingCredits = env('TICKET_PRICE_CREDITS') - Auth::user()->credits;
+            $fail('You do not have enough credits. Missing: ' .$missingCredits);
+        }
+    }
+}
